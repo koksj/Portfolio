ca17afcced0b19e9912def2ff7408b0ea7f7a37e
Stefan Kok <kok.sjx@gmail.com>
2018-10-03 00:28:22 +0200

    First commit of project structure

---
+++ thorntail-with-jca/thorntail/src/main/java/io/example/thorntail/rest/RestApplication.java
@@ -0,0 +1,9 @@
+package io.example.thorntail.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class RestApplication extends Application {
+
+}
